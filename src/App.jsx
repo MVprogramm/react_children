cafb2f73596ba1398126824cb517cd3e963e2ccc
Michael Valiahmetov <michael.valiahmetov@gmail.com>
2022-06-22 16:58:54 +0300

    add project

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Dialog from "./Dialog.jsx";
+
+class App extends React.Component {
+  state = {
+    isOpen: false
+  }
+
+  hideDialog = () => {
+    this.setState({
+      isOpen: false
+    })
+  }
+
+ showDialog = () => {
+    this.setState({
+      isOpen: true
+    })
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <button className="btn" onClick={this.showDialog}>Show</button>
+        <Dialog 
+          isOpen={this.state.isOpen} 
+          title="Recommendation"
+          onClose={this.hideDialog}
+        >
+          <p>
+            Use immutable array methods
+            to work with data.  It will 
+            help to avoid bugs.
+          </p>
+        </Dialog>
+      </div>
+    );
+  }
+};
+
+export default App;
+
+
